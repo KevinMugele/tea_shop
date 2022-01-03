b64fe11e3774dad62ea9788a0054219cc6503326
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-03 15:31:25 -0500

    Write model and migration for customer table

---
+++ db/migrate/20220103202402_create_customers.rb
@@ -0,0 +1,15 @@
+class CreateCustomers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+
+      t.timestamps
+    end
+  end
+end
