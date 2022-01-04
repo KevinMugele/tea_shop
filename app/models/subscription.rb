1c8a59687ea2cd806ad51b1ef48abad80c62f530
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-04 17:28:27 -0500

    Add enum for subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,4 +7,7 @@ class Subscription < ApplicationRecord
   validates :price, presence: true, numericality: true
   validates :frequency, presence: true
   validates :status, presence: true
+
+  # enum frequency: { 'Weekly' => 0, 'Monthly' => 1, 'Annually' => 2 }
+  # enum status: { 'Active' => 0, 'Cancelled' => 1 }
 end
