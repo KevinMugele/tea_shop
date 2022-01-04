52402e2eb493a2553d29a9e374a5bb5b27fa2daa
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-04 15:59:39 -0500

    Add validations for customer and subscriptions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,11 @@
 class Customer < ApplicationRecord
+  has_many :subscriptions, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,10 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates :customer_id, presence: true, numericality: true
+  validates :tea_id, presence: true
+  validates :price, presence: true, numericality: true
+  validates :frequency, presence: true
+  validates :status, presence: true
 end
