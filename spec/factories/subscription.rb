ccb40a50db97fb53a12d22c69e30363acfd85d5c
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-04 17:28:12 -0500

    Update factory

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -1,9 +1,6 @@
 FactoryBot.define do
   factory :subscription do
     title { Faker::Tea.variety }
-    description { Faker::Tea.type }
-    temperature { rand(80..100) }
-    brew_time { rand(1..5) }
     price { Faker::Number.decimal(l_digits: 2) }
     frequency { %w[Weekly Monthly Annually].sample }
     status { %w[Active Cancelled].sample }
