ae1c5edeaa00512762f0b7b051d8c0ca3f3a90c5
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-04 16:00:24 -0500

    Add subscriptions controller with index, create, and update methods

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def index
+    customer = Customer.find(params[:customer_id])
+    subscriptions = customer.subscriptions
+    json_response(SubscriptionSerializer.new(subscriptions))
+  end
+
+  def create
+    customer = Customer.find(params[:customer_id])
+    subscription = Subscription.create!(subscription_params)
+    json_response(SubscriptionSerializer.new(subscription))
+  end
+
+  def update
+    customer = Customer.find(params[:customer_id])
+    subscription = Subscription.find(params[:id])
+    customer.subscriptions.find(subscription.id)
+    subscription.update!(subscription_params)
+    json_response(SubscriptionSerializer.new(subscription))
+  end
+
+  def subscription_params
+    params.permit(:title, :tea_id, :price, :frequency, :status, :customer_id)
+  end
+end
