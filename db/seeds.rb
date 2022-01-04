93c7b7fdc9fac3591d9801880735d68d2f6fc54f
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-04 17:27:37 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+include FactoryBot::Syntax::Methods
+
+Subscription.destroy_all
+Customer.destroy_all
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+customer_1 = create(:customer)
+customer_2 = create(:customer)
+
+tea_1 = create(:tea)
+tea_2 = create(:tea)
+tea_3 = create(:tea)
+
+subscription_1 = create(:subscription, customer: customer_1, tea: tea_1, status: 'Active')
+subscription_2 = create(:subscription, customer: customer_1, tea: tea_2, status: 'Active')
+subscription_3 = create(:subscription, customer: customer_2, tea: tea_3, status: 'Cancelled')
