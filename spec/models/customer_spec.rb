6d9071bcab92923f3ecf4ee308f4587a9e0f506d
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-06 18:41:35 -0500

    Add relationship and validation testing for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'relationships' do
+    it { should have_many(:subscriptions).dependent(:destroy) }
   end
 
   describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
   end
 end
