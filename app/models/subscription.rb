1de4a784426735452590ebb122c4ce9df38fc050
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-06 18:38:30 -0500

    Change status and frequency to enums

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,6 +8,6 @@ class Subscription < ApplicationRecord
   validates :frequency, presence: true
   validates :status, presence: true
 
-  # enum frequency: { 'Weekly' => 0, 'Monthly' => 1, 'Annually' => 2 }
-  # enum status: { 'Active' => 0, 'Cancelled' => 1 }
+  enum frequency: { 'Weekly' => 0, 'Monthly' => 1, 'Annually' => 2 }
+  enum status: { 'Active' => 0, 'Cancelled' => 1 }
 end
