40de554f9ec723adb6ada33689d8361cce6bcd43
Kevin Mugele <kevin.mugele@gmail.com>
2022-01-06 18:42:11 -0500

    Fix spacing

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
   describe 'relationships' do
-     it { should belong_to(:customer) }
+    it { should belong_to(:customer) }
   end
 
   describe 'validations' do
